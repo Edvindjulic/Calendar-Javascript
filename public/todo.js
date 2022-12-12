5361436622901f1ed3a6ba5d149ea008fd629c14
stenbumling <marcusrosin@outlook.com>
2022-12-12 14:44:41 +0100

    Prevented cases of empty todos

--- public/todo.js
+++ public/todo.js
@@ -7,6 +7,8 @@ function main () {
     const userInputDate = document.getElementById("new-todo-date");
     userInputTodo.value = "";
     userInputDate.value = "";
+    const userInputErrorTodo = document.getElementById("new-todo-activity-error-message");
+    const userInputErrorDate = document.getElementById("new-todo-date-error-message");
 
     // Variablar
     const addNewItemOtherDays = document.getElementById("tlod-activities");
@@ -16,18 +18,35 @@ function main () {
 
     // Grundläggande funktion för att lägga till en todo
         addNewItemButton.addEventListener("click", () => {
-        const newTodo = document.createElement("li");
-        const newTodoContent = document.createTextNode(userInputDate.value + " – " + userInputTodo.value);
-        newTodo.appendChild(newTodoContent);
-        addNewItemToday.appendChild(newTodo);
-        userInputTodo.value = "";
-        userInputDate.value = "";
+
+        userInputErrorDate.style.display = "none";
+        userInputErrorTodo.style.display = "none";
+
+        // Lägg till todo om inputfälten inte är tomma
+        if (userInputTodo.value != "" && userInputDate.value != "") {
+            const newTodo = document.createElement("li");
+            const newTodoContent = document.createTextNode(userInputDate.value + " – " + userInputTodo.value);
+            newTodo.appendChild(newTodoContent);
+            addNewItemToday.appendChild(newTodo);
+            userInputTodo.value = "";
+            userInputDate.value = "";
+
+        // Varna om inputfält är tomt
+        } else {
+            if (userInputTodo.value == "") {
+                userInputErrorTodo.style.display = "block";
+            } 
+            if (userInputDate.value == ""){
+                userInputErrorDate.style.display = "block";
+            }
+        }
     });
 
     // Funktion för användaren att avbryta skapande av en todo
     addNewItemButtonCancel.addEventListener("click", () => {
         userInputTodo.value = "";
         userInputDate.value = "";
+        userInputErrorDate.style.display = "none";
+        userInputErrorTodo.style.display = "none";
     });
-
 }
