833c05421ca41128abb1a25b58f5a27aab7a31cf
Moa Hedendahl <moahedendahl@gmail.com>
2022-12-16 14:11:01 +0100

    wrote jsdocs, added css for calendar

--- public/newcalendar.js
+++ public/newcalendar.js
@@ -1,62 +1,64 @@
+/**
+ * A global variabel that changes if you change months by pressing buttons
+ */
 let nav = 0;
 
+/**
+ * A global list of all the days in a week
+ */
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-// Laddas vid start samt när man navigerar mellan månader
+/**
+ * Loads at start and every time you change month
+ */
 function loadCalendar() {
     const date = new Date();
 
-    // Bestämmer vilken månad det är baserat på var i navigeringen man är (nav ändras med 1 om man trycker på pilarna)
     if (nav !== 0) {
         date.setMonth(new Date().getMonth() + nav);
       }
-    // Sparar nuvarande datum i variablar
+
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
 
-    // Räknar ut vilken veckodag första dagen i given månad är
     const firstDayOfTheMonth = new Date(year, month, 1);
-    
-    // Räknar ut hur många dagar det är i given månad
     const daysInAMonth = new Date(year, month + 1, 0).getDate();
-    // Förvandlar veckodagen för första dagen i månaden till en sträng
     const dateToString = firstDayOfTheMonth.toLocaleDateString('en-us', {
         weekday: 'long',
         year: 'numeric',
         month: 'numeric',
         day: 'numeric',
     });
-    console.log(dateToString);
-    // Bestämmer hur många rutor i kalendergriden som är tomma innan första dagen i månaden
-    // (t.ex. tre tomma rutor om första dagen i månaden är en torsdag)
-    const emptyDaySquare = weekdays.indexOf(dateToString.split(', ')[0]);
-    console.log(emptyDaySquare);
 
-    document.getElementById('monthAndYear').innerText = 
-    `${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
-    
-    // Rensar kalenderdiven innan den ritas upp på nytt
+    const emptyDaySquare = weekdays.indexOf(dateToString.split(', ')[0]);
+
+    const monthAndYear = document.getElementById('monthAndYear');
+    monthAndYear.innerText = `${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+    monthAndYear.classList.add("current-month");
     
     renderDaySquare(emptyDaySquare, daysInAMonth, day, month, year);
-    
-
 }
 
-// Renderar varje dag i given månad
+/**
+ * Renders every square in a month
+ * @param {number} emptyDaySquare 
+ * @param {number} daysInAMonth 
+ * @param {number} day 
+ * @param {number} month 
+ * @param {number} year 
+ */
 function renderDaySquare(emptyDaySquare, daysInAMonth, day, month, year) {
     const calendar = document.getElementById("month-calendar");
     calendar.innerHTML = "";
     
     for(let i = 1; i <= emptyDaySquare + daysInAMonth; i++) {
-        // Skapar en div med klassen "day" (d.v.s. en ruta i kalendern)
         const daySquare = document.createElement("div");
         daySquare.classList.add("day-square");
-        // Kontrollerar om rutan som ska ritas upp ska vara tom eller inte
+
         if (i > emptyDaySquare) {
-            // Ritar ut dagens nummer i rutan
             daySquare.innerText = i - emptyDaySquare;
-            // Om dagen som ritas upp är dagens datum så får den id-stylingen "currentDay" (blir ljusblå)
+
             if (i - emptyDaySquare === day && nav === 0) {
                 daySquare.id = "today";
             }
@@ -64,14 +66,15 @@ function renderDaySquare(emptyDaySquare, daysInAMonth, day, month, year) {
         } else {
             daySquare.classList.add("empty")
         }
-            
-        // Lägger till rutan i DOM
         calendar.appendChild(daySquare);
     }
 }
 
+/**
+ * Function that makes <> buttons clickable, so that the month changes. 
+ * When < is clicked nav decreases with 1, and when > is clicked nav increases with 1
+ */
 function initButtons() {
-    // Gör byt månad-knapparna funktionella
     document.getElementById('nextButton').addEventListener('click', () => {
       nav++;
       loadCalendar();
