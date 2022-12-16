ffea6a86e63f30bf6c26d916250ae96a00948e0b
stenbumling <marcusrosin@outlook.com>
2022-12-16 14:22:11 +0100

    Added function for user to complete (and undo completion of) todo

--- public/todo.js
+++ public/todo.js
@@ -23,30 +23,51 @@ function main () {
         // Lägg till aktivitet om inputfälten inte är tomma
         if (newItemActivity != "" && newItemDate != "") {
     
+            // Skapar ny aktivitet
             const newItem = document.createElement("li");
             newItem.classList.add("item");
 
+            // Skapar ny datumbricka
             const badgeItem = document.createElement("span");
             badgeItem.classList.add("item-badge");
             const badgeItemContent = document.createTextNode(newItemDate);
 
+            // Skapar en div för styling
             const newItemContentDiv = document.createElement("span");
             newItemContentDiv.classList.add("item-text");
             const newItemContent = document.createTextNode(newItemActivity);
 
+            // Skapar en borttagningsknapp
             const buttonDeleteItem = document.createElement("button");
-            buttonDeleteItem.classList.add("item-delete-button", "material-symbols-outlined");
+            buttonDeleteItem.classList.add("item-buttons", "material-symbols-outlined");
             buttonDeleteItem.setAttribute("data-cy", "delete-todo-button");
             buttonDeleteItem.innerHTML = "delete";
             buttonDeleteItem.addEventListener('click', deleteItem);
             
+            // Skapar en avklarningsknapp
+            const buttonCompleteItem = document.createElement("button");
+            buttonCompleteItem.classList.add("item-buttons", "material-symbols-outlined");
+            buttonCompleteItem.innerHTML = "done";
+            buttonCompleteItem.addEventListener('click', completeItem);
+
+            // Skapar en ångraknapp (för avklarad aktivitet)
+            const buttonUndoItem = document.createElement("button");
+            buttonUndoItem.classList.add("item-buttons", "material-symbols-outlined");
+            buttonUndoItem.innerHTML = "undo";
+            buttonUndoItem.style.display = "none";
+            buttonUndoItem.addEventListener('click', completeItem);
+
+            // Samlar ihop allt och pushar till DOM
             badgeItem.appendChild(badgeItemContent);
             newItem.appendChild(badgeItem);
             newItemContentDiv.appendChild(newItemContent);
             newItem.appendChild(newItemContentDiv);
+            newItem.appendChild(buttonUndoItem);
+            newItem.appendChild(buttonCompleteItem);
             newItem.appendChild(buttonDeleteItem);
             todoListToday.appendChild(newItem);
             
+            // Rensar inputfälten efter tilläggning
             todoListEmptyTextToday.style.display = "none";
             userInputActivity.value = "";
             userInputDate.value = "";
@@ -66,6 +87,21 @@ function main () {
 
         if (todoListToday.innerText == "") {
             todoListEmptyTextToday.style.display = "block";
+        }
+    }
+
+    // Markerar en aktivitet som avklarad eller oavklarad
+    function completeItem() {
+        let item = this.parentNode.firstChild.nextSibling;
+
+        if (item.style.textDecoration == "line-through") {
+            item.style.textDecoration = "none";
+            this.style.display = "none";
+            this.nextSibling.style.display = "block";
+        } else {
+            item.style.textDecoration = "line-through";
+            this.style.display = "none";
+            this.previousSibling.style.display = "block";
         }
     }
 }
