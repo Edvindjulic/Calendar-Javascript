3ef43cc466ae73fa66a6ba96acd2daf89315603d
Moa Hedendahl <moahedendahl@gmail.com>
2022-12-21 21:27:52 +0100

    Now the right amount of holidays is shown, still not IN calendar

--- public/holidays.js
+++ public/holidays.js
@@ -1,10 +1,12 @@
 async function getHoliday() {
+
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
   const response = await fetch(`https://sholiday.faboul.se/dagar/v2.1/${year}/${month}`);
+
   const holidays = await response.json();
   const listOfHolidays = []
 
@@ -13,13 +15,11 @@ async function getHoliday() {
       
       listOfHolidays.push(holiday);
 
-      listOfHolidays.forEach(function(holiday) {
-        const daySquareHoliday = document.createElement('div');
+      const daySquareHoliday = document.createElement('div');
         daySquareHoliday.innerHTML = holiday.helgdag;
         daySquareHoliday.style.backgroundColor = "red";
         calendar.appendChild(daySquareHoliday);
-        console.log(holiday);
-      });
-  }
+    } 
+    
 }
 }
