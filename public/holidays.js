bc314e9261d4d80194c49b3db321fa5db6718622
Moa Hedendahl <moahedendahl@gmail.com>
2022-12-21 19:04:51 +0100

    holidays are displayd now, not correct amount and not where they should be

--- public/holidays.js
+++ public/holidays.js
@@ -1,5 +1,10 @@
 async function getHoliday() {
-  const response = await fetch(`https://sholiday.faboul.se/dagar/v2.1/2022/12`);
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  const response = await fetch(`https://sholiday.faboul.se/dagar/v2.1/${year}/${month}`);
   const holidays = await response.json();
   const listOfHolidays = []
 
@@ -7,26 +12,75 @@ async function getHoliday() {
     if (holiday.helgdag) {
       
       listOfHolidays.push(holiday);
+
+      listOfHolidays.forEach(function(holiday) {
+        const calendarHoliday = document.createElement('div');
+        calendarHoliday.innerHTML = holiday.helgdag;
+        calendarHoliday.style.backgroundColor = "red";
+        calendar.appendChild(calendarHoliday);
+      });
     }
   }
-
-  renderHolidays(listOfHolidays);
-  console.log(listOfHolidays);
 }
 
+// async function getHoliday() {
+//   const response = await fetch(`https://sholiday.faboul.se/dagar/v2.1/2022/12`);
+//   const holidays = await response.json();
+//   const listOfHolidays = []
 
-function renderHolidays(listOfHolidays) {
-  for (const i = 0; i <= listOfHolidays; i++) {
-    const date = new Date();
-    const day = date.getDate();
-    console.log("hallå");
+//   const date = new Date()
 
-    if (listOfHolidays[i].date == day) {
-      const div = document.createElement("div");
-      div.style.backgroundColor = "red";
-      console.log("hallå");
-      document.appendChild(div);
-    }
 
-  }
-}
+//   for (const holiday of holidays.dagar) {
+//     if (holiday.helgdag) {
+      
+//       listOfHolidays.push(holiday);
+
+//       listOfHolidays.forEach(function(holiday) {
+//         const daySquareHoliday = document.createElement("div");
+
+//         if (todoListLocalStorage == null) {
+
+//         } else {
+//             addHolidayToCalendar(daySquareHoliday, daySquare.id);
+//         }
+//         daySquare.appendChild(daySquareHoliday);
+//         calendar.appendChild(daySquare);
+
+//   console.log(listOfHolidays);
+//       });
+//     }
+//   }
+// }
+
+// function addHolidayToCalendar(daySquareHoliday, day) {
+//   for (var i = 0; i < todoListLocalStorage.length; i++) {;
+//     if (todoListLocalStorage[i].date == day) {
+//         if (todoListLocalStorage[i].completed == true) {
+            
+//         } else if (calendarTodo.innerHTML == "") {
+//             calendarTodo.innerHTML = 1;
+//         } else {
+//             calendarTodo.innerHTML++;;
+//         }
+//     }
+// }
+// }
+
+
+
+// function renderHolidays(listOfHolidays) {
+//   for (const i = 0; i <= listOfHolidays; i++) {
+//     const date = new Date();
+//     const day = date.getDate();
+//     console.log("hallå");
+
+//     if (listOfHolidays[i].date == day) {
+//       const div = document.createElement("div");
+//       div.style.backgroundColor = "red";
+//       console.log("hallå");
+//       document.appendChild(div);
+//     }
+
+//   }
+// }
